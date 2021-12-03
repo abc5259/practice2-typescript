ee20b7b0caeb29ef219fa95ac697eedc98912dcf
LeeJaeHoon <dlwogns3413@naver.com>
2021-12-03 22:27:00 +0900

    learn typesciprt number, boolean, string, object, Array, tuple, enum

---
+++ app.js
@@ -0,0 +1,15 @@
+var job;
+(function (job) {
+    job[job["AUTHOR"] = 0] = "AUTHOR";
+    job[job["STUDENT"] = 1] = "STUDENT";
+})(job || (job = {}));
+console.log(job.AUTHOR);
+var person = {
+    name: "leejaehoon",
+    age: 30,
+    hobbies: ["sports", "cooking"],
+    role: [2, "hi", 3]
+};
+var ary;
+ary = ["ad", "dd"];
+console.log(person.name);
