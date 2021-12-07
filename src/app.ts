6767f6bce874617335bc438fe7d9bb02ff510c07
LeeJaeHoon <dlwogns3413@naver.com>
2021-12-08 01:13:06 +0900

    learn typescript class

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,23 @@
-let userInput: unknown; //사용자가 어떤 값을 넣을지모름
-let userName: string;
+class Department {
+  private employees: string[] = []; //class내에서만 접근가능
 
-userInput = 5; //lt's work!
-userInput = "max"; //lt's work!
+  constructor(private readonly id: number, public name: string) {}
+  describe(this: Department) {
+    console.log(`Department id: ${this.id} name: ${this.name}`);
+  }
 
-if (typeof userInput === "string") {
-  userName = userInput; //lt's work!
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
+
+const accounting = new Department(1, "accounting");
+
+accounting.addEmployee("max");
+accounting.describe();
+accounting.printEmployeeInformation();
