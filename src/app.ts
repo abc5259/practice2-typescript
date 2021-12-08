1848ebd461ede062eb4dbeeebfa9d4527f42afc3
LeeJaeHoon <dlwogns3413@naver.com>
2021-12-09 00:40:37 +0900

    learn static

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,11 @@ class Department {
   private employees: string[] = []; //class내에서만 접근가능
 
   constructor(private readonly id: number, public name: string) {}
+
+  static createEmployee(name: string) {
+    return { name };
+  }
+
   describe(this: Department) {
     console.log(`Department id: ${this.id} name: ${this.name}`);
   }
@@ -17,7 +22,8 @@ class Department {
 }
 
 const accounting = new Department(1, "accounting");
-
+const employee = Department.createEmployee("jaehoon");
+console.log(employee);
 accounting.addEmployee("max");
 accounting.describe();
 accounting.printEmployeeInformation();
